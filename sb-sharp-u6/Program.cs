41ff89260daacb191abd108e1386bf7b7b47a2f6
yomma <furbolg174@gmail.com>
2024-04-04 00:38:11 +0500

    task done

--- sb-sharp-u6/Program.cs
+++ sb-sharp-u6/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             char key = '1';
+            int lastRecord = 0;
+            string source = "EmployeeList.txt";
+            string[] userInputFields = 
+            {
+                "Ф.И.О.",
+                "возраст",
+                "рост",
+                "дата рождения",
+                "место рождения"
+            };
 
             Console.WriteLine(
                 "Программа \"Справочник\" успешно загружена." +
@@ -13,22 +23,69 @@
                 "\n2. Добавить запись" +
                 "\nДля выхода нажмите любую другую клавишу"
                 );
-
             key = Console.ReadKey(true).KeyChar;
 
             switch (key)
             {
                 case '1':
-                //метод выводящий данные
+                    ShowEmployeeList();
+                    break;
                 case '2':
-                //метод запускающий процесс добавления записи в конец файла
+                    AddRecord();
+                    break;
                 default:
                     break;
             }
 
-            static void Get()
-            {
+            Console.ReadKey();
 
+            void ShowEmployeeList()
+            {
+                Console.Clear();
+                if (File.Exists(source))
+                {
+                   using(StreamReader streamReader = new StreamReader(source))
+                    {
+                        string line;
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            string[] data = line.Split('#');
+                            Console.WriteLine(
+                                $"Запись №{data[0]} " +
+                                $"от {data[1]}\n\t" +
+                                $"{data[2]}\n\t" +
+                                $"Возраст: {data[3]}\n\t" +
+                                $"Рост: {data[4]}\n\t" +
+                                $"Дата рождения: {data[5]}\n\t" +
+                                $"Место рождения: {data[6]}\n"
+                                );
+                        }
+
+                    }
+                }
+                else
+                    Console.WriteLine("Пока нет ни одной записи");
+            }
+
+            void AddRecord()
+            {
+                Console.Clear();
+                if (File.Exists(source))
+                    lastRecord = File.ReadAllLines(source).Length;
+                else
+                    lastRecord=0;
+
+                using (StreamWriter streamWriter = new StreamWriter(source, true))
+                {
+                    streamWriter.Write($"{lastRecord + 1}#{DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}");
+                    for (int i = 0; i < userInputFields.Length; i++)
+                    {
+                        Console.Clear();
+                        Console.Write($"Введите {userInputFields[i]}: ");
+                        streamWriter.Write("#" + Console.ReadLine());
+                    }
+
+                }
             }
         }
     }
